2909850b0e944709f679c3d8ecbccf875c480137
Robert Bigelow <rfbigelow@mac.com>
2018-11-04 17:22:06 -0800

    Initial Commit

---
+++ Markov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Markov
+//
+//  Created by Robert Bigelow on 11/4/18.
+//  Copyright © 2018 Robert Bigelow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
