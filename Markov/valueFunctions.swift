214b59891db537e4b996c1e2a2b9d3132a9e2cf8
Robert Bigelow <rfbigelow@mac.com>
2018-11-12 12:39:28 -0800

    Simplified action value function.

--- Markov/valueFunctions.swift
+++ Markov/valueFunctions.swift
@@ -18,28 +18,22 @@ func getStateValue<TModel: MarkovDecisionProcess, TPolicy: Policy>(
             let actionProb = policy.getProbability(fromState: state, ofTaking: action)
             let actionReward = mdp.getReward(fromState: state, forTakingAction: action)
             let (nextState, _) = mdp.transition(fromState: state, byTakingAction: action)
-            let nextStateValue = v(nextState)
-            sum += actionProb * (actionReward + discount * nextStateValue)
+            sum += actionProb * (actionReward + discount * v(nextState))
         }
     }
     return sum
 }
 
-func getActionValue<TModel: MarkovDecisionProcess, TPolicy: Policy>(
+func getActionValue<TModel: MarkovDecisionProcess>(
     forState state: TModel.State,
     withAction action: TModel.Action,
     mdp: TModel,
-    policy: TPolicy,
     discount: Double,
-    q:(TModel.State, TModel.Action) -> Reward) -> Reward where TModel.State == TPolicy.State, TModel.Action == TPolicy.Action {
+    v:(TModel.State) -> Reward) -> Reward {
     var sum = 0.0
     if let transitions = mdp.getTransitions(fromState: state, forTakingAction: action) {
         for (transition, probability) in transitions {
-            var maxQ = 0.0
-            if let nextActions = mdp.getActions(forState: transition.state) {
-                maxQ = nextActions.map({ q(transition.state, $0) }).max() ?? maxQ
-            }
-            sum += probability * (transition.reward + discount * maxQ)
+            sum += probability * (transition.reward + discount * v(transition.state))
         }
     }
     return sum
